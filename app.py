0736867be79ec2dfdf8f347dc182fe4831db060a
oumtoufik17-code <oumtoufik17@gmail.com>
2025-08-24 19:46:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import base64
 import requests
 import uuid
 
-from flask import abort, Flask, render_template, request, redirect, jsonify, make_response, url_for
+from flask import abort, Flask, render_template, request, redirect, jsonify, make_response, url_for, session
 from datetime import date, datetime, timezone
 from email.mime.text import MIMEText
 
